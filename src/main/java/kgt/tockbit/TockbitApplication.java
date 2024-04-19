06549c8a9b2df69e7f919c9d2eb968f3b351a98d
Guntae Kim <dkfkskgks@naver.com>
2024-04-20 06:49:32 +0900

    프로젝트 초기화

---
+++ src/main/java/kgt/tockbit/TockbitApplication.java
@@ -0,0 +1,13 @@
+package kgt.tockbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TockbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TockbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kgt/tockbit/TockbitApplicationTests.java
@@ -0,0 +1,13 @@
+package kgt.tockbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TockbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
