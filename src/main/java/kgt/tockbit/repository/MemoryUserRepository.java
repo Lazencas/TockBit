25df85e57d7325dbae888a5bc38529cbfb0b69ff
Guntae Kim <dkfkskgks@naver.com>
2024-04-21 00:47:08 +0900

    테스트 후 초기화 함수
    junit 테스트 후 초기화 기능 구현

--- src/main/java/kgt/tockbit/repository/MemoryUserRepository.java
+++ src/main/java/kgt/tockbit/repository/MemoryUserRepository.java
@@ -46,4 +46,8 @@ public class MemoryUserRepository implements UserRepository{
     public List<User> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
 }
--- src/test/java/kgt/tockbit/repository/MemoryUserRepositoryTest.java
+++ src/test/java/kgt/tockbit/repository/MemoryUserRepositoryTest.java
@@ -1,8 +1,11 @@
 package kgt.tockbit.repository;
 
 import kgt.tockbit.domain.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +13,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MemoryUserRepositoryTest {
 
     MemoryUserRepository repository = new MemoryUserRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -19,8 +27,24 @@ public class MemoryUserRepositoryTest {
         repository.save(user);
 
         User result = repository.findById(user.getId()).get();
-        assertThat(user).isEqualTo(null);
-
+        assertThat(user).isEqualTo(result);
 
     }
+
+    @Test
+    public void findByName(){
+        User user1 = new User();
+        user1.setName("same");
+        repository.save(user1);
+
+        User user2 = new User();
+        user2.setName("same2");
+        repository.save(user2);
+
+        User result = repository.findByName("same").get();
+
+        assertThat(result).isEqualTo(user1);
+
+    }
+
 }
