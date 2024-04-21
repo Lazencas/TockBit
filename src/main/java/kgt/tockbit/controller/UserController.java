e16f7228757ec4fde06a182f370d27a35b5829ef
Guntae Kim <dkfkskgks@naver.com>
2024-04-22 03:03:09 +0900

    유저목록 폼, 로직 작성
    나중에 회원가입을 쉽게 확인 할 수 있게 유저목록폼, 및 관련 로직 작성 완료

--- src/main/java/kgt/tockbit/controller/UserController.java
+++ src/main/java/kgt/tockbit/controller/UserController.java
@@ -4,9 +4,12 @@ import kgt.tockbit.domain.User;
 import kgt.tockbit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 //@RequestMapping("/api")
@@ -35,6 +38,14 @@ public class UserController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/users")
+    public String list(Model model){
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        return "users/userList";
+    }
+
 
     @GetMapping("/user/login-page")
     public String loginPage() {
