6e67528e10c5389b5a3e383efb1f1ca8ce2b91fb
Guntae Kim <dkfkskgks@naver.com>
2024-04-24 10:00:39 +0900

    쿠키 및 jwt 정리
    필요없는 부분의 테스트 코드 삭제

--- src/main/java/kgt/tockbit/controller/UserController.java
+++ src/main/java/kgt/tockbit/controller/UserController.java
@@ -140,63 +140,7 @@ public class UserController {
         return "users/login";
     }
 
-    @GetMapping("/create-cookie")
-    public String createCookie(HttpServletResponse res) {
-        addCookie("Robbie Auth", res);
-        return "createCookie";
-    }
 
-    @ResponseBody
-    @GetMapping("/get-cookie")
-    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
-        System.out.println("value = " + value);
-
-        return "getCookie : " + value;
-    }
-
-    @GetMapping("/create-jwt")
-    public String createJwt(HttpServletResponse res) {
-        // Jwt 생성
-        String token = jwtUtil.createToken("Robbie");
-
-        // Jwt 쿠키 저장
-        jwtUtil.addJwtToCookie(token, res);
-
-        return "createJwt : " + token;
-    }
-    @ResponseBody
-    @GetMapping("/get-jwt")
-    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
-        // JWT 토큰 substring
-        String token = jwtUtil.substringToken(tokenValue);
-
-        // 토큰 검증
-        if(!jwtUtil.validateToken(token)){
-            throw new IllegalArgumentException("Token Error");
-        }
-
-        // 토큰에서 사용자 정보 가져오기
-        Claims info = jwtUtil.getUserInfoFromToken(token);
-        // 사용자 username
-        String username = info.getSubject();
-        System.out.println("username = " + username);
-        return "getJwt : " + username;
-    }
-
-    public static void addCookie(String cookieValue, HttpServletResponse res) {
-        try {
-            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
-
-            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
-            cookie.setPath("/");
-            cookie.setMaxAge(30 * 60);
-
-            // Response 객체에 Cookie 추가
-            res.addCookie(cookie);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
 
     @GetMapping("/sendEmail")
     public ResponseEntity<String> sendEmail(@RequestParam String toEmail){
