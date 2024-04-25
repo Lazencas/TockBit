bd31364a660ddb3229b0327668127c4310ab6606
Guntae Kim <dkfkskgks@naver.com>
2024-04-25 15:03:22 +0900

    #11 close 팔로우한 유저의 활동을 뉴스피드에 표시 구현 완료
    추후에 like 테이블 따로 분리해야 함.

--- src/main/java/kgt/tockbit/service/NewsFeedService.java
+++ src/main/java/kgt/tockbit/service/NewsFeedService.java
@@ -1,8 +1,13 @@
 package kgt.tockbit.service;
 
+import kgt.tockbit.domain.Activity;
+import kgt.tockbit.domain.User;
 import kgt.tockbit.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 public class NewsFeedService {
@@ -11,6 +16,17 @@ public class NewsFeedService {
     public NewsFeedService(ActivityRepository activityRepository) {
         this.activityRepository = activityRepository;
     }
+
+    public List<Activity> getNewsFeed(User user){
+        List<Activity> activities = activityRepository.findActivitiesByUserOrFollower(user, user);
+
+        activities.sort(Comparator.comparing(Activity::getCreatedAt).reversed());
+
+        return activities;
+
+    }
+
+
 
 
 
