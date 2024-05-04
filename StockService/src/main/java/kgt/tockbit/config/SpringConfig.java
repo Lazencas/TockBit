45bc88659d6efc3d89138ec6e181a3713fcf0897
Guntae Kim <dkfkskgks@naver.com>
2024-05-04 10:30:02 +0900

    주식서비스 초기화 및 전체 설정

---
+++ StockService/src/main/java/kgt/tockbit/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package kgt.tockbit.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringConfig {
+
+}
---
+++ StockService/src/test/java/kgt/tockbit/service/UserServiceIntegrationTest.java
@@ -0,0 +1,79 @@
+package kgt.tockbit.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class UserServiceIntegrationTest {
+   @Autowired
+   UserService userService;
+   @Autowired
+   UserRepository userRepository;
+
+
+    @Test
+    @Commit
+    void 회원가입() {
+        //given
+        User user = new User();
+        user.setName("김건태2");
+        user.setEmail("abcde@zab.com");
+        user.setPassword("123456");
+        user.setGreet("헬ss노맨");
+
+        //when
+       String saveEmail = userService.join(user);
+
+        //then
+        User findUser = userService.findOne(saveEmail).get();
+        assertThat(user.getEmail()).isEqualTo(findUser.getEmail());
+
+    }
+    @Test
+    public void 중복이메일회원() {
+        //given
+        User user1 = new User();
+        user1.setEmail("aa@ab.com");
+
+        User user2 = new User();
+        user2.setEmail("aa@ab.com");
+
+        //when
+        userService.join(user1);
+        assertThrows(IllegalStateException.class, ()-> userService.join(user2));
+
+        //then
+
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void findUsers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
